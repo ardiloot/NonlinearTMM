21047182c7781554871769c8dc9ff5d0154cbd8c
Ardi <ardi.loot@ut.ee>
2017-03-21 22:40:54 +0200

    Updated docs makefiles
    - Changed build directory in order to publish to gh-pages

--- docs/make.bat
+++ docs/make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=.
-set BUILDDIR=_build
+set BUILDDIR=../../NonlinearTMM-docs
 set SPHINXPROJ=NonlinearTMM
 
 if "%1" == "" goto help
