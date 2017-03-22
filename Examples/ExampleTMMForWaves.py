ce4f057a9b2c15e55f400645f4695431b5e6df33
Ardi <ardi.loot@ut.ee>
2017-03-22 16:26:38 +0200

    Added TMM for waves example
    - Unified code style with ExampleTMM

--- Examples/ExampleTMMForWaves.py
+++ Examples/ExampleTMMForWaves.py
@@ -2,7 +2,7 @@ import numpy as np
 import pylab as plt
 from NonlinearTMM import TMM, Material
 
-def CalcSpp():
+def CalcSppGaussianBeam():
     # Parameters
     #---------------------------------------------------------------------------
     wl = 532e-9  # Wavelength
@@ -11,8 +11,11 @@ def CalcSpp():
     metalD = 50e-9  # Metal film thickness
     enhLayer = 2  # Measure enhancment in the last layer
     ths = np.radians(np.linspace(0.0, 80.0, 500))  # Angle of incidences
-    xs = np.linspace(-2e-6, 2e-6, 200)  # Field calculation coordinates
-    zs = np.linspace(-2e-6, 2e-6, 201)  # Field calculation coordinates
+    xs = np.linspace(-50e-6, 50e-6, 200)  # Field calculation coordinates
+    zs = np.linspace(-25e-6, 5e-6, 201)  # Field calculation coordinates
+    waveType = "gaussian"  # Wave type
+    pwr = 10e-3  # Beam power [W]
+    w0 = 10e-6  # Beam waist size
     
     # Specify materials
     #---------------------------------------------------------------------------
@@ -27,62 +30,53 @@ def CalcSpp():
     tmm.AddLayer(metalD, ag)
     tmm.AddLayer(float("inf"), dielectric)
     
+    # Init wave params
+    tmm.wave.SetParams(waveType = waveType, w0 = w0, pwr = pwr, \
+                       dynamicMaxX = False, maxX = xs[-1])
+    
     # Solve
     #---------------------------------------------------------------------------
     
     # Calculate reflection, transmission and field enhancement
     betas = np.sin(ths) * prism.GetN(wl).real
-    sweepRes = tmm.Sweep("beta", betas, outEnh = True, layerNr = enhLayer) 
+    sweepRes = tmm.WaveSweep("beta", betas, outEnh = True, layerNr = enhLayer) 
     
     # Calculate fields at the reflection dip (excitation of SPPs)
     betaMaxEnh = betas[np.argmax(sweepRes.enh)]
     tmm.Solve(beta = betaMaxEnh)
-    
-    # Calculate 1D fields
-    fields1D = tmm.GetFields(zs)
-    
-    # Calculate 2D fields
-    fields2D = tmm.GetFields2D(zs, xs)
+    fields2D = tmm.WaveGetFields2D(zs, xs)
     
     # Ploting
     #---------------------------------------------------------------------------
     plt.figure()
+    ax1 = plt.subplot2grid((2, 2), (0, 0))
+    ax2 = plt.subplot2grid((2, 2), (1, 0))
+    ax3 = plt.subplot2grid((2, 2), (0, 1), rowspan = 2)
     thMaxEnh = np.arcsin(betaMaxEnh / prism.GetN(wl).real)
     
     # Reflection / transmission
-    plt.subplot(221)
-    plt.plot(np.degrees(ths), sweepRes.Ir, label = "R")
-    plt.plot(np.degrees(ths), sweepRes.It, label = "T")
-    plt.axvline(np.degrees(thMaxEnh), ls = "--", color = "red", lw = 1.0)
-    plt.xlabel(r"$\theta$ ($\degree$)")
-    plt.ylabel(r"Intensity (a.u)")
-    plt.legend()
+    ax1.plot(np.degrees(ths), 1e3 * sweepRes.Pi, label = r"$P_i$")
+    ax1.plot(np.degrees(ths), 1e3 * sweepRes.Pr, label = r"$P_r$")
+    ax1.plot(np.degrees(ths), 1e3 * sweepRes.Pt, label = r"$P_t$")
+    ax1.axvline(np.degrees(thMaxEnh), ls = "--", color = "red", lw = 1.0)
+    ax1.set_xlabel(r"$\theta$ ($\degree$)")
+    ax1.set_ylabel(r"Power (mW)")
+    ax1.legend()
     
     # Field enhancement
-    plt.subplot(222)
-    plt.plot(np.degrees(ths), sweepRes.enh)
-    plt.axvline(np.degrees(thMaxEnh), ls = "--", color = "red", lw = 1.0)
-    plt.xlabel(r"$\theta$ ($\degree$)")
-    plt.ylabel(r"Field enhancement")
-    
-    # Fields 1D
-    plt.subplot(223)
-    plt.plot(1e6 * zs, fields1D.E[:, 0].real, label = r"$E_x$")
-    plt.plot(1e6 * zs, fields1D.E[:, 2].real, label = r"$E_z$")
-    plt.plot(1e6 * zs, np.linalg.norm(fields1D.E, axis = 1), label = r"‖E‖")
-    plt.xlabel(r"z (μm)")
-    plt.ylabel(r"(V/m)")
-    plt.legend()
+    ax2.plot(np.degrees(ths), sweepRes.enh)
+    ax2.axvline(np.degrees(thMaxEnh), ls = "--", color = "red", lw = 1.0)
+    ax2.set_xlabel(r"$\theta$ ($\degree$)")
+    ax2.set_ylabel(r"Field enhancement")
     
     # Fields 2D
-    plt.subplot(224)
-    plt.pcolormesh(1e6 * zs, 1e6 * xs, fields2D.Ez.real.T, rasterized = True)
-    plt.xlabel(r"z (μm)")
-    plt.ylabel(r"x (μm)")
-    plt.colorbar(label = r"$E_z$ (V/m)")
+    cm = ax3.pcolormesh(1e6 * zs, 1e6 * xs, 1e-3 * fields2D.EN.real.T, vmax = 5e1)
+    ax3.set_xlabel(r"z (μm)")
+    ax3.set_ylabel(r"x (μm)")
+    plt.colorbar(cm, label = r"$‖E‖$ (kV/m)")
 
     plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
-    CalcSpp()
+    CalcSppGaussianBeam()
