8c274681f11a5fec30f9b0824f68441254100f9f
Ardi <ardi.loot@ut.ee>
2017-06-05 14:44:07 +0300

    Improved SPDC tests
    - Testing power integration through waves

--- Tests/test_SPDC.py
+++ Tests/test_SPDC.py
@@ -50,6 +50,9 @@ def SpdcPowerQuantum(wlP1, wlP2, betaP1, betaP2, nF, chi2, crystalL, pwrP1, dwl,
         
     return res
 
+@pytest.fixture(params = [0.0, 0.3])
+def betaP1(request):
+    return request.param
 
 @pytest.fixture(params = [0.1e-3, 1e-3])
 def crystalD(request):
@@ -67,6 +70,10 @@ def n1(request):
 def n2(request):
     return request.param
 
+@pytest.fixture(params = ["planewave", "gaussian"])
+def waveP1Type(request):
+    return request.param
+
 @pytest.fixture()
 def tmmParams(crystalD, pwrP1, n1, n2):
     # Define params
@@ -75,6 +82,9 @@ def tmmParams(crystalD, pwrP1, n1, n2):
     polP1 = "s"
     polP2 = "s"
     polGen = "s"
+    w0 = 1e-3
+    Ly = 1e-3
+    I0 = pwrP1  / (w0 * Ly)
     
     deltaThetaSpdc = np.radians(0.5)
     solidAngleSpdc = 7.61543549467e-05
@@ -92,7 +102,7 @@ def tmmParams(crystalD, pwrP1, n1, n2):
     # Init SecondOrderNLTMM
     tmm = SecondOrderNLTMM(mode = "spdc", deltaWlSpdc = deltaWlSpdc, \
                            solidAngleSpdc = solidAngleSpdc, deltaThetaSpdc = deltaThetaSpdc)
-    tmm.P1.SetParams(wl = wlP1, pol = polP1, I0 = pwrP1)
+    tmm.P1.SetParams(wl = wlP1, pol = polP1, I0 = I0)
     tmm.P2.SetParams(wl = wlP2, pol = polP2, overrideE0 = True, E0 = 1.0)
     tmm.Gen.SetParams(pol = polGen)
     
@@ -101,12 +111,11 @@ def tmmParams(crystalD, pwrP1, n1, n2):
     tmm.AddLayer(crystalD, crystal)
     tmm.AddLayer(float("inf"), dielectric)
     
-    return tmm, prism, chi2
+    return tmm, prism, chi2, w0, Ly
     
     
-def test_SPDCIntensity(tmmParams):
-    tmm, nF, chi2 = tmmParams
-    betaP1 = 0.0
+def test_SPDCIntensity(tmmParams, betaP1):
+    tmm, nF, chi2, w0, Ly = tmmParams
     betasP2 = np.linspace(0.0, 0.9, 10000)
     betasP1 = np.ones_like(betasP2) * betaP1
     pwrP1 = tmm.P1.I0
@@ -116,6 +125,59 @@ def test_SPDCIntensity(tmmParams):
     sr = tmm.Sweep("beta", betasP1, betasP2)
     quantumSpdc = SpdcPowerQuantum(tmm.P1.wl, tmm.P2.wl, betaP1, betasP2, \
         lambda wl: nF.GetN(wl).real, chi2, crystalD, pwrP1, tmm.deltaWlSpdc, tmm.solidAngleSpdc, tmm.deltaThetaSpdc)
+    coef = 1.0 / np.cos(np.arcsin(betasP1 / nF.GetN(tmm.P1.wl).real)) 
     
-    np.testing.assert_allclose(quantumSpdc, sr.Gen.It, rtol = 1e-6)
+    np.testing.assert_allclose(quantumSpdc, sr.Gen.It * coef, rtol = 1e-6)
+
+def test_SPDCPwrs(tmmParams, waveP1Type, betaP1):
+    tmm, nF, chi2, w0, Ly = tmmParams 
+    betasP2Test = np.linspace(0.0, 0.9, 10000)
+    betasP1Test = np.ones_like(betasP2Test) * betaP1
+    
+    # Waves
+    pwrP1 = tmm.P1.I0 * w0 * Ly
+    waveP1Params = {"waveType": waveP1Type, "pwr": pwrP1, "w0": w0, "Ly": Ly, "dynamicMaxXCoef": 10, "nPointsInteg": 100, "maxPhi": np.radians(10.0)}
+    waveP2Params = {"waveType": "spdc", "w0": w0, "Ly": Ly, "overrideE0": True, "nPointsInteg": 100}
+    tmm.P1.wave.SetParams(**waveP1Params)
+    tmm.P2.wave.SetParams(**waveP2Params)
+
+    # Find maximum signal
+    srTest = tmm.Sweep("beta", betasP1Test, betasP2Test)
+    index = np.argmax(srTest.Gen.It)
+    betaP2 = betasP2Test[index]
+    
+    # Manual integration
+    # --------------------------------------------------------------------------
+    
+    # Find betaP2 integration range
+    wlGen = srTest.wlsGen[0]
+    n0Gen = nF.GetN(wlGen).real
+    maxBetaGen = srTest.betasGen[index]
+    kz0Gen = (2.0 * np.pi / wlGen) * np.sqrt(n0Gen ** 2 - maxBetaGen ** 2)
+    deltaKxP2 = kz0Gen / (2.0 * n0Gen) * tmm.deltaThetaSpdc
+    deltaBetaP2 = deltaKxP2 / (2.0 * np.pi / tmm.P2.wl)
+    
+    # Integrate
+    betasP2Int = np.linspace(betaP2 - deltaBetaP2, betaP2 + deltaBetaP2, 100)
+    betasP1Int = np.ones_like(betasP2Int) * betaP1
+    kxsP2Int = betasP2Int * (2.0 * np.pi / tmm.P2.wl)
+    srInt = tmm.Sweep("beta", betasP1Int, betasP2Int)
+    
+    # Int power
+    pwrSPDC = np.trapz(srInt.Gen.It, kxsP2Int)
+    
+    # Wave integration
+    # --------------------------------------------------------------------------
+    
+    tmm.P1.beta = betaP1
+    tmm.P2.beta = betaP2
+    pwrSpdcW = tmm.WaveGetPowerFlows(2)[0]
+    beamArea = tmm.P1.wave.beamArea
+    
+    # Testing
+    # --------------------------------------------------------------------------
+    np.testing.assert_allclose(beamArea * pwrSPDC, pwrSpdcW, rtol = 1e-2)
+    
+    
+    
 
